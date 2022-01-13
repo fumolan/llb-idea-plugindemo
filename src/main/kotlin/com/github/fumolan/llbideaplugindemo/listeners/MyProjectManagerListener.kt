8bdbb9926b387157c7ee40ecbee6918ad864e4fc
GitHub Action <action@github.com>
2022-01-13 11:41:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/fumolan/llbideaplugindemo/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/fumolan/llbideaplugindemo/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.fumolan.llbideaplugindemo.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.fumolan.llbideaplugindemo.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/fumolan/llbideaplugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/fumolan/llbideaplugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.fumolan.llbideaplugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fumolan.llbideaplugindemo.MyBundle
 
 class MyApplicationService {
 
